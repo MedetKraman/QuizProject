b763d30aa32d3e15fa76ed98618e6a3aa2e3217c
Краман Медет <medetkraman@gmail.com>
2019-12-29 17:06:23 +0600

    Updating SimpleQuizCreateDTO

--- SimpleQuiz/Models/DTOs/SimpleQuizCreateDTO.cs
+++ SimpleQuiz/Models/DTOs/SimpleQuizCreateDTO.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SimpleQuiz.Models.DTOs {
     public class SimpleQuizCreateDTO {
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public string Desc { get; set; }
 
-        public List<(string qst, List<string> opts, string answ)> Questions { get; set; }
+        public List<(Guid id, string qst, List<string> opts, string answ)> Questions { get; set; }
     }
 }
