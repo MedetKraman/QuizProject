533fdd7b8f9a504e43132c9bbf9b7eaa4a764d77
Краман Медет <medetkraman@gmail.com>
2019-12-29 15:59:54 +0600

    Modified Student Models

--- SimpleQuiz/Models/StudentView.cs
+++ SimpleQuiz/Models/StudentView.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 
 namespace SimpleQuiz.Models {
-    public sealed class StudentView {
-        public StudentView(string nickname) {
-            Nickname = nickname.IsSome() ? nickname : throw new ArgumentNullException(nameof(nickname));
+    public abstract class StudentView {
+        public abstract string Nickname { get; }
+    }
+
+    internal sealed class StudentWithNick : StudentView {
+        public readonly string nickname;
+        internal StudentWithNick(string nickname) {
+            this.nickname = nickname.IsSome() ? nickname : throw new ArgumentNullException(nameof(nickname));
         }
 
-        public string Nickname { get; }
+        public override string Nickname => throw new NotImplementedException();
+    }
+
+    internal sealed class StudentWithFIO : StudentView {
+        public readonly FIOModel FIOModel;
+        public StudentWithFIO(string lastname, string firstname, string? fathername) {
+            FIOModel = new FIOModel(lastname, firstname, fathername);
+        }
+
+        public override string Nickname => FIOModel.FullFIO;
     }
 }
