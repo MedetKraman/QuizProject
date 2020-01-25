8871d114a0b2ed106ea8a5a1e1da91a5151fe7ad
Краман Медет <medetkraman@gmail.com>
2020-01-25 18:42:58 +0600

    Added student code login

---
+++ QuizApp.QTEST/Data/SimpleQuizService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizApp.QTEST.Data {
+    public class SimpleQuizService {
+    }
+}
