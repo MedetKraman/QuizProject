9a26364e52d6fd3480a4711965b86c7886b23747
Краман Медет <medetkraman@gmail.com>
2020-01-26 14:12:44 +0600

    student quiz question page

--- QuizApp.QTEST/Data/DemoSimpleQuizService.cs
+++ QuizApp.QTEST/Data/DemoSimpleQuizService.cs
@@ -3,13 +3,15 @@ using SimpleQuiz.Models;
 using SimpleQuiz.Models.DTOs;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace QuizApp.QTEST.Data {
     public class DemoSimpleQuizService : ISimpleQuizService {
 
         public Task<SimpleQuizView> GetBy(Guid quizId) {
+
+            var ansId = Guid.NewGuid();
+
             SimpleQuizCreateDTO dto = new SimpleQuizCreateDTO {
                 QuizId = Guid.NewGuid(),
                 QuizTitle = "Quiz title",
@@ -17,8 +19,44 @@ namespace QuizApp.QTEST.Data {
                 QuizQuestions = new List<(Guid id, string qst, Dictionary<Guid, string> opts, Guid answ)> {
                     (Guid.NewGuid(), 
                      "Question 1", 
-                     new Dictionary<Guid, string> { { Guid.NewGuid(), "Option 1" }, { Guid.NewGuid(), "Option 2" }, { Guid.NewGuid(), "Option 3" }, { Guid.NewGuid(), "Option 4" } }, 
-                     Guid.NewGuid()),
+                     new Dictionary<Guid, string> { { ansId, "Option 1" }, { Guid.NewGuid(), "Option 2" }, { Guid.NewGuid(), "Option 3" }, { Guid.NewGuid(), "Option 4" } },
+                     ansId),
+                    (Guid.NewGuid(), 
+                     "Question 1", 
+                     new Dictionary<Guid, string> { { ansId, "Option 1" }, { Guid.NewGuid(), "Option 2" }, { Guid.NewGuid(), "Option 3" }, { Guid.NewGuid(), "Option 4" } },
+                     ansId),
+                    (Guid.NewGuid(), 
+                     "Question 1", 
+                     new Dictionary<Guid, string> { { ansId, "Option 1" }, { Guid.NewGuid(), "Option 2" }, { Guid.NewGuid(), "Option 3" }, { Guid.NewGuid(), "Option 4" } },
+                     ansId),
+                    (Guid.NewGuid(), 
+                     "Question 1", 
+                     new Dictionary<Guid, string> { { ansId, "Option 1" }, { Guid.NewGuid(), "Option 2" }, { Guid.NewGuid(), "Option 3" }, { Guid.NewGuid(), "Option 4" } },
+                     ansId),
+                    (Guid.NewGuid(), 
+                     "Question 1", 
+                     new Dictionary<Guid, string> { { ansId, "Option 1" }, { Guid.NewGuid(), "Option 2" }, { Guid.NewGuid(), "Option 3" }, { Guid.NewGuid(), "Option 4" } },
+                     ansId),
+                    (Guid.NewGuid(), 
+                     "Question 1", 
+                     new Dictionary<Guid, string> { { ansId, "Option 1" }, { Guid.NewGuid(), "Option 2" }, { Guid.NewGuid(), "Option 3" }, { Guid.NewGuid(), "Option 4" } },
+                     ansId),
+                    (Guid.NewGuid(), 
+                     "Question 1", 
+                     new Dictionary<Guid, string> { { ansId, "Option 1" }, { Guid.NewGuid(), "Option 2" }, { Guid.NewGuid(), "Option 3" }, { Guid.NewGuid(), "Option 4" } },
+                     ansId),
+                    (Guid.NewGuid(), 
+                     "Question 1", 
+                     new Dictionary<Guid, string> { { ansId, "Option 1" }, { Guid.NewGuid(), "Option 2" }, { Guid.NewGuid(), "Option 3" }, { Guid.NewGuid(), "Option 4" } },
+                     ansId),
+                    (Guid.NewGuid(), 
+                     "Question 1", 
+                     new Dictionary<Guid, string> { { ansId, "Option 1" }, { Guid.NewGuid(), "Option 2" }, { Guid.NewGuid(), "Option 3" }, { Guid.NewGuid(), "Option 4" } },
+                     ansId),
+                    (Guid.NewGuid(), 
+                     "Question 1", 
+                     new Dictionary<Guid, string> { { ansId, "Option 1" }, { Guid.NewGuid(), "Option 2" }, { Guid.NewGuid(), "Option 3" }, { Guid.NewGuid(), "Option 4" } },
+                     ansId),
                 },
 
                 CuratorId = Guid.NewGuid(),
